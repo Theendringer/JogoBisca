6371a588bd374a95fe7de8ee94cdc7a888fc9fde
Gabriela1997 <gabrielamarcolano@hotmail.com>
2023-05-01 20:29:18 -0300

    metodoscarta

--- src/br/unisales/jogoBisca/models/Carta.java
+++ src/br/unisales/jogoBisca/models/Carta.java
@@ -1,6 +1,38 @@
 package br.unisales.jogoBisca.models;
 
 public class Carta {
+	private String nype;
+	private String face;
+	private int peso;
 
+	public Carta(String nype, String face, int peso) {
+		this.nype = nype;
+		this.face = face;
+		this.peso = peso;
+	}
+
+	public String getNype() {
+		return nype;
+	}
+
+	public void setNype(String nype) {
+		this.nype = nype;
+	}
+
+	public String getFace() {
+		return face;
+	}
+
+	public void setFace(String face) {
+		this.face = face;
+	}
+
+	public int getPeso() {
+		return peso;
+	}
+
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
 	
 }
