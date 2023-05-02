13a8b4d5acd5f56815dcac2ce8305cb927cf161f
Gabriella1997 <gabrielamarcolano@hotmail.com>
2023-05-02 18:16:19 -0300

    metodosjogador

--- src/br/unisales/jogoBisca/models/Jogador.java
+++ src/br/unisales/jogoBisca/models/Jogador.java
@@ -1,9 +1,38 @@
 package br.unisales.jogoBisca.models;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 
 
 public class Jogador {
-	//teste
-	//Teste de usuario	
+	List<Carta> cartas;
+	//List<Carta> cartaJogada;
+	Carta jogada;
+	Carta selecionada;
+
+	public Jogador() {
+		super();
+		cartas = new ArrayList<>();
+	}
+
+	public void comprarCarta(Baralho b) {
+		Carta c = b.comprarCarta();
+		cartas.add(c);
+		
+		//System.out.println(c.toString());
+	}
+
+	public int contarPontos() {
+		int acc = 0;
+		for (Carta carta : cartas) {
+			acc += carta.getPeso(); // acc = acc + carta.getPeso();
+		}
+		
+		//System.out.println(acc);//teste 
+		return acc;
+	}
+		
 	
 }
