683df150642291b5929ff49dc9a01ccef532796c
rafascalf <rafael.scalfoni@souunisales.com.br>
2023-05-02 20:14:07 -0300

    main

--- src/br/unisales/jogoBisca/Programa.java
+++ src/br/unisales/jogoBisca/Programa.java
@@ -1,9 +1,15 @@
 package br.unisales.jogoBisca;
+import java.util.Scanner;
+import br.unisales.jogoBisca.models.*;
 
 public class Programa {
 	
 	public static void main(String [] args){ 
-	    System.out.println("Teste"); 
+	   
+	    Scanner sc = new Scanner(System.in);
+			
+		Baralho b = new Baralho();
+		b.embaralhar();
 	    
 	}
 	
