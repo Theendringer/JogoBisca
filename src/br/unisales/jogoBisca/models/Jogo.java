e8427806b8173b80a30b78ec66b279a7f7a943d3
Caac1984 <Caac1984>
2023-05-02 22:50:01 -0300

    get trunfo

--- src/br/unisales/jogoBisca/models/Jogo.java
+++ src/br/unisales/jogoBisca/models/Jogo.java
@@ -1,11 +1,18 @@
 package br.unisales.jogoBisca.models;
 import java.util.ArrayList;
-
+import java.io.IOException;
+import java.util.List;
+import java.util.Iterator;
 
 public class Jogo {
+	List<Carta> rodada;
 	public Jogo() {
 		super();
 		rodada = new ArrayList<>();
+	}
+	
+	public void getTrunfo() {
+		System.out.println(trunfo);
 	}
 	
 }
