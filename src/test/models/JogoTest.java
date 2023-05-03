6155cc39723cb30fe19af38f69a9622747a9df14
Caac1984 <Caac1984>
2023-05-02 23:00:34 -0300

    test jogo

--- src/test/models/JogoTest.java
+++ src/test/models/JogoTest.java
@@ -13,5 +13,26 @@ import br.unisales.jogoBisca.models.Jogo;
 
 public class JogoTest {
 	
+	@Test
+	void testeJogo2() {
+Baralho b = new Baralho();
+		
+		Jogador j1 = new Jogador();
+		Jogador j2 = new Jogador();
+		Jogador j3 = new Jogador();
+		Jogador j4 = new Jogador();
+		
+		Jogo jogo = new Jogo();
+		
+		b.embaralhar();
+		
+		
+		jogo.iniciaJogo(b, j1, j2, j3, j4);
+		
+		j1.contarPontos();
+		
+		
+		assertNotEquals(11, j1.contarPontos());	
+	}
 	
 }
